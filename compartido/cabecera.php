5b27043b9a43e7154ff55ba8a24ccca8be8557b5
Micaelo <prodomitor@gmail.com>
2020-11-10 14:28:44 +0100

    header style improves

--- compartido/cabecera.php
+++ compartido/cabecera.php
@@ -3,20 +3,23 @@ session_start();
 echo "
 <header class='header'>
     <nav>
-        <h1>Ilerna M07</h1>";
-        if(isset($_SESSION['nombre'])) {
-            echo "<h2>Has entrado como ".$_SESSION['nombre']."</h2>";
-        } else {
-            echo "<h2>Has entrado como huésped</h2>";
-        }
-        echo "<ul class='nav'>
-            <li><button class='button button-blue'><a href='/ilerna/index.php'>Inicio</a></button></li>
-            <li><button class='button button-green'><a href='/ilerna/usuario/usuarios.php'>Usuarios</a></button></li>
-            <li><button class='button button-green'><a href='/ilerna/noticia/noticias.php'>Ver Todas las noticias</a></button></li>";
+        <div class='nav-top-level'>
+            <h1 class='logo'>Ilerna M07</h1>";
             if(isset($_SESSION['nombre'])) {
-                echo    "<li><button class='button button-green'><a href='/ilerna/usuario/form_usuario.php'>Crear usuario</a></button></li>
-                        <li><button class='button button-green'><a href='/ilerna/noticia/nueva.php'>Crear noticias</a></button></li>
-                        <li><button class='button button-red'><a href='/ilerna/usuario/logout.php'>Salir</a></button></li>";
+                echo "<h2>Has entrado como ".$_SESSION['nombre']."</h2>";
+            } else {
+                echo "<h2>Has entrado como huésped</h2>";
+            }
+        echo "</div>
+        
+        <ul class='nav'>
+            <li><a class='nav-link' href='/ilerna/index.php'>Inicio</a></li>
+            <li><a class='nav-link' href='/ilerna/usuario/usuarios.php'>Usuarios</a></li>
+            <li><a class='nav-link' href='/ilerna/noticia/noticias.php'>Ver Todas las noticias</a></li>";
+            if(isset($_SESSION['nombre'])) {
+                echo    "<li><a  class='nav-link' href='/ilerna/usuario/form_usuario.php'>Crear usuario</a></li>
+                        <li><a  class='nav-link' href='/ilerna/noticia/nueva.php'>Crear noticias</a></li>
+                        <li><a  class='nav-link' href='/ilerna/usuario/logout.php'>Salir</a></li>";
             } else {
                 echo "<li><button class='button button-green'><a href='/ilerna/loguin.php'>Autorizarse</a></button></li>";
             }
